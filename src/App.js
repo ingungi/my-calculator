2677a148197ef448863baf3ec7ce3b0bad4e50b9
ingungi <ian.mwau@gmail.com>
2023-04-25 15:15:33 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const mathOps = ['/', '*', '+', '-'];
+
+  const updateCalc = (val) => {
+    if (val === '=') {
+      if (calc === '') {
+        return;
+      }
+      try {
+        setResult(eval(calc));
+      } catch {
+        setResult("Error");
+      }
+      setCalc('');
+      return;
+    }
+    if (val === 'AC') {
+      setResult('');
+      setCalc('');
+      return;
+    }
+    const lastChar = calc[calc.length - 1];
+    if (mathOps.includes(val) && mathOps.includes(lastChar)) {
+      setCalc(calc.slice(0, -1) + val);
+    } else {
+      setCalc(calc + val);
+    }
+  };
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i <= 9; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+      <div className="wrapper">
+        <div className="screen">
+          {result ? <span>({result})</span> : ''}
+          {calc || '0'}
+        </div>
+        <div className="inputs">
+          <div className="operators">
+            <button onClick={() => updateCalc('*')}>×</button>
+            <button onClick={() => updateCalc('-')}>−</button>
+            <button onClick={() => updateCalc('+')}>+</button>
+            <button onClick={() => updateCalc('/')}>÷</button>
+            <button onClick={() => updateCalc('AC')}>AC</button>
+          </div>
+          <div className="digits">
+            {createDigits()}
+            <button onClick={() => updateCalc('0')}>0</button>
+            <button onClick={() => updateCalc('.')}>.</button>
+            <button onClick={() => updateCalc('=')}>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
